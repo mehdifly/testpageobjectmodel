3c4785365b200fd22ddc46bd61474dcf8f37b725
Mehdi Gourija <m.gourija@gmail.com>
2020-12-09 12:29:17 +0100

    add changee

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,5 +1,6 @@
 package com.crm.qa.testcases;
 
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -15,6 +16,7 @@ public class LoginPageTest extends TestBase{
 	
 	LoginPage loginPage;
 	HomePage  homePage;
+	Logger log = Logger.getLogger(LoginPageTest.class);
 	
 	public LoginPageTest() {
 		super();
@@ -22,24 +24,39 @@ public class LoginPageTest extends TestBase{
 	
 	@BeforeMethod
 	public void setUp() {
+		log.info("****************************** Starting test cases execution  *****************************************");
 		
 		initialization();
-		
+		log.info("launching chrome broswer");
 		loginPage = new LoginPage();
+		log.info("entering application URL");
+		log.warn("Hey this just a warning message");
+		log.fatal("hey this is just fatal error message");
+		log.debug("this is debug message");
 	}
 	
 	@Test(priority =1)
 	public void loginPageTitleTest() {
 		
+		log.info("****************************** starting test case *****************************************");
+		log.info("****************************** freeCrmTitleTest *****************************************");
 		String title = loginPage.validationLoginPageTitle();
+		log.info("login page title is--->"+title);
 		Assert.assertEquals(title, "#1 Free CRM customer relationship management software cloud");
+		log.info("****************************** ending test case *****************************************");
+		log.info("****************************** freeCrmTitleTest *****************************************");
 	}
 	
 	@Test(priority =2)
 	public void crmLogoImageTest() {
 		
+		log.info("****************************** starting test case *****************************************");
+		log.info("****************************** freemCRMLogoTest *****************************************");
+		
 		boolean flag = loginPage.validateCrmImage();
 		Assert.assertTrue(flag);
+		log.info("****************************** ending test case *****************************************");
+		log.info("****************************** freemCRMLogoTest *****************************************");
 		}
 	
 	@Test(priority =3)
